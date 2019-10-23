8c28bff0e5e48d6f30581f338fb1b50de3336559
PC <PC@192.168.1.180>
2019-10-23 09:18:38 +0200

    Añadido la clase password y el jsp para recuperar la contraseña del usuario

--- src/java/Email/Email.java
+++ src/java/Email/Email.java
@@ -13,55 +13,92 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 /**
- * 
+ *
  * @author Celia
- * @since 18/10/2019
- * Creación de la clase de email para usarla para la recuperación de contraseñas
+ * @since 18/10/2019 Creación de la clase de email para usarla para la
+ * recuperación de contraseñas
  */
-
 public class Email {
-    
-    public void enviarCorreo(String de, String clave, String para, String mensaje, String asunto){
-            try{
-            
-                String host = "smtp.gmail.com";
-                
-                Properties prop = System.getProperties();
-                
-                prop.put("mail.smtp.starttls.enable","true");
-                prop.put("mail.smtp.host", host);
-                prop.put("mail.smtp.user",de);
-                prop.put("mail.smtp.password", clave);
-                prop.put("mail.smtp.port",587);
-                //prop.put("mail.smtp.port",465);
-                prop.put("mail.smtp.auth","true");
-                
-                Session sesion = Session.getDefaultInstance(prop,null);
-                
-                MimeMessage message = new MimeMessage(sesion);
-                
-                message.setFrom(new InternetAddress(de));
 
-                
-                message.setRecipient(Message.RecipientType.TO, new InternetAddress(para));
-                
-                message.setSubject(asunto);
-                message.setText(mensaje);
-                
-                Transport transport = sesion.getTransport("smtp");
-                
-                transport.connect(host,de,clave);
-                
-                transport.sendMessage(message, message.getAllRecipients());
-                
-                transport.close();
-                
-                
-            }catch(Exception e){
-                e.printStackTrace();
-            }
-        
+    public void enviarCorreo(String de, String clave, String para, String mensaje, String asunto) {
+        try {
+
+            String host = "smtp.gmail.com";
+
+            Properties prop = System.getProperties();
+
+            prop.put("mail.smtp.starttls.enable", "true");
+            prop.put("mail.smtp.host", host);
+            prop.put("mail.smtp.user", de);
+            prop.put("mail.smtp.password", clave);
+            prop.put("mail.smtp.port", 587);
+            //prop.put("mail.smtp.port",465);
+            prop.put("mail.smtp.auth", "true");
+
+            Session sesion = Session.getDefaultInstance(prop, null);
+
+            MimeMessage message = new MimeMessage(sesion);
+
+            message.setFrom(new InternetAddress(de));
+
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(para));
+
+            message.setSubject(asunto);
+            message.setText(mensaje);
+
+            Transport transport = sesion.getTransport("smtp");
+
+            transport.connect(host, de, clave);
+
+            transport.sendMessage(message, message.getAllRecipients());
+
+            transport.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
-    
-}
 
+    public boolean enviarCorreo(String para, String mensaje, String asunto) {
+        boolean enviado = false;
+        try {
+            String host = "smtp.gmail.com";
+            String de = "auxiliardaw2@gmail.com";
+            String clave = "Chubaca20";
+
+            Properties prop = System.getProperties();
+
+            prop.put("mail.smtp.starttls.enable", "true");
+            prop.put("mail.smtp.host", host);
+            prop.put("mail.smtp.user", de);
+            prop.put("mail.smtp.password", clave);
+            prop.put("mail.smtp.port", "587");
+            prop.put("mail.smtp.auth", "true");
+
+            Session sesion = Session.getDefaultInstance(prop, null);
+
+            MimeMessage message = new MimeMessage(sesion);
+
+            message.setFrom(new InternetAddress(de));
+
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(para));
+
+            message.setSubject(asunto);
+            message.setText(mensaje);
+
+            Transport transport = sesion.getTransport("smtp");
+
+            transport.connect(host, de, clave);
+
+            transport.sendMessage(message, message.getAllRecipients());
+
+            transport.close();
+
+            enviado = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return enviado;
+    }
+}
---
+++ web/recuperarpasswd.jsp
@@ -0,0 +1,60 @@
+<%-- 
+    Document   : recuperarpasswd
+    Created on : 23-oct-2019, 8:42:36
+    Author     : daw206
+--%>
+
+<%@page import="Email.Passwords"%>
+<%@page import="java.util.LinkedList"%>
+<%@page import="Datos.Profesor"%>
+<%@page import="Email.Email"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Recuperar contraseña</title>
+        <link rel="stylesheet" type="text/css" href="css/miestilo.css">
+    </head>
+    <body>
+        <form name="recuperarpasswd" action="" method="POST">
+            <input type="email" name="email" placeholder="Escriba su email">
+            <input type="submit" name="recuperar" value="Recuperar Contraseña">
+            <%
+                Email mail = new Email();
+                LinkedList<Profesor> listausuarios = (LinkedList) session.getAttribute("usuarios");
+                
+                if (request.getParameter("recuperar") !=null) {
+                    String email = request.getParameter("email");
+                    String mensaje ="";
+                    String pass = "";
+                    boolean enviado = false;
+                    
+                    pass = Passwords.getPassword(8);
+                    
+                    mensaje = "Hemos cambiado la contraseña del usuario " + email + " por " + pass;
+                    
+                    enviado = mail.enviarCorreo(request.getParameter("email"), mensaje, "Recuperar Contraseña");
+                    
+                    if (enviado) {
+                        for (Profesor p : listausuarios) {
+                            if (p.getUsuario().equals(email)) {
+                                p.setPasswd(pass);
+                                session.setAttribute("usuarios", listausuarios);
+                            }
+                        }
+                        
+                        out.print("El mensaje para recuperar la contraseña ha sido enviado");
+                    } else {
+                        out.print("No hemos podido enviar el correo. Por favor, vuelva a intentarlo");
+                    }
+                    
+                }
+                
+
+                %>
+                
+                <a href="index.jsp"><input type="button" name="volver" value="Volver"></a>
+        </form>
+    </body>
+</html>
